b6d393b733729b9bdba6930d70c4c1d456cbfb44
dawidpietras <dawid.pietras.91@gmail.com>
2020-01-18 14:18:36 +0100

    rebuild Cards class and start Suits class

--- poker-game/poker.py
+++ poker-game/poker.py
@@ -3,6 +3,9 @@ used_cards = []
 
 
 class Cards:
+    '''
+    Class returns particular cards, hands of players and deck
+    '''
 
     value_list = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2]
     suit_list = ['clubs', 'diamonds', 'hearts', 'spades']
@@ -10,22 +13,44 @@ class Cards:
     def __init__(self):
         self.player_hand = []
 
-    def getRandomCards(self):
-        player_hand = []
+    def getRandomCard(self):
+        card = []  # one card e.g ['spades', 'A']
+        rand_suit = random.choice(self.suit_list)
+        rand_value = random.choice(self.value_list)
+        card.append(rand_suit)
+        card.append(rand_value)
+        return card
+
+    def getPlayerHand(self):
         i = 0
         while i < 2:
-            rand_suit = random.choice(self.suit_list)
-            rand_value = random.choice(self.value_list)
-
-            if rand_value not in used_cards and rand_suit not in used_cards:
-                self.player_hand.append(rand_suit)
-                self.player_hand.append(rand_value)
+            player_card = self.getRandomCard()
+            if player_card not in used_cards:
+                self.player_hand.append(player_card)
                 i += 1
-        used_cards.append(player_hand)
-        return self.player_hand
+                used_cards.append(player_card)
+        return self.player_hand  # two cards, entire hand like [['spades', 'A'], ['diamonds', 'A']]
+
+    def getDeck(self):
+        deck = []  # list of 5 cards deck [['hearts', 9],['diamonds', 'K'],['clubs', 'J'],['clubs', 'A'],['clubs', 'Q']]
+        i = 0
+        while i < 5:
+            deck_card = self.getRandomCard()
+            if deck_card not in used_cards:
+                deck.append(deck_card)
+                i += 1
+                used_cards.append(deck_card)
+        return deck
 
 
-playerOneCards = Cards().getRandomCards()
-playerTwoCards = Cards().getRandomCards()
+class Suits:
+    pass
+
+
+playerOneCards = Cards().getPlayerHand()
+playerTwoCards = Cards().getPlayerHand()
 print('First Player Cards: {}  Second Player Cards: {}'.format(playerOneCards, playerTwoCards))
-print(used_cards)
+deck = Cards().getDeck()
+print('Current Deck: {}'.format(deck))
+print('Used Cards: {}'.format(used_cards))
+
